0abecf73e98cf7edd570f9853ed3d5b0110108bb
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-22 00:09:10 +0700

    update

--- src/main/java/SalaryCalculator.java
+++ src/main/java/SalaryCalculator.java
@@ -1,16 +1,19 @@
 public class SalaryCalculator {
     public double salaryMultiplier(int daysSkipped) {
-        if(daysSkipped < 0 ) return 0;
+        if (daysSkipped < 0)
+            return 0;
         return daysSkipped >= 5 ? 1 - 0.15 : 1;
     }
 
     public int bonusMultiplier(int productsSold) {
-        if(productsSold < 0 ) return 0;
+        if (productsSold < 0)
+            return 0;
         return productsSold >= 20 ? 13 : 10;
     }
 
     public double bonusForProductsSold(int productsSold) {
-        if(productsSold < 0 ) return 0;
+        if (productsSold < 0)
+            return 0;
         return bonusMultiplier(productsSold) * productsSold; // 13 * 77
     }
 
@@ -21,6 +24,6 @@ public class SalaryCalculator {
 
     public static void main(String[] args) {
         SalaryCalculator salaryCalculator = new SalaryCalculator();
-        System.out.println("tinh luong: " + salaryCalculator.finalSalary(2,77));
+        System.out.println("tinh luong: " + salaryCalculator.finalSalary(2, 77));
     }
 }
