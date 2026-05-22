96b6341a75b81d508b83fe9950f43e4a3f7c9adc
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-22 17:09:56 +0700

    update

---
+++ src/main/java/LogLevel.java
@@ -0,0 +1,19 @@
+public enum LogLevel {
+    TRC,
+    DBG,
+    INF,
+    WRN,
+    ERR,
+    FTL
+
+}
+/*
+ * TRC (trace)
+ * DBG (debug)
+ * INF (info)
+ * WRN (warning)
+ * ERR (error)
+ * FTL (fatal)
+ * 
+ * 
+ */
