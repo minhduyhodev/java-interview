33452301ff25d3ae8ff79ecfe35d3505dce1c758
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-24 15:16:11 +0700

    Define Warrior and Wizard classes with their respective methods and properties

--- src/main/java/DialingCodes.java
+++ src/main/java/DialingCodes.java
@@ -52,4 +52,5 @@ public class DialingCodes {
             this.code.put(code, country);
         }
     }
+
 }
--- src/main/java/Fighter.java
+++ src/main/java/Fighter.java
@@ -9,6 +9,45 @@ class Fighter {
     }
 }
 
-// TODO: define the Warrior class
+class Warrior extends Fighter {
 
-// TODO: define the Wizard class
+    @Override
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 10 : 6;
+    }
+
+}
+
+class Wizard extends Fighter {
+    boolean spellPrepared = false;
+
+    @Override
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return !spellPrepared;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return spellPrepared ? 12 : 3;
+    }
+
+    public void prepareSpell() {
+        spellPrepared = true;
+    }
+
+}
