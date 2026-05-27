6926fded381e7b3be3f76620331faf289d1a121b
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-27 23:14:16 +0700

    update

--- src/main/java/TimfromMarketing.java
+++ src/main/java/TimfromMarketing.java
@@ -2,21 +2,18 @@ public class TimfromMarketing {
 
     public String print(Integer id, String name, String department) {
 
-//        Badge badge = new Badge();
-//        badge.print(734, "Ernest Johnny Payne", "Strategic Communication");
-// => "[734] - Ernest Johnny Payne - STRATEGIC COMMUNICATION"
-        if(id == null && department != null){
-            return  name + " - " + department.toUpperCase();
+        if (id == null && department != null) {
+            return name + " - " + department.toUpperCase();
         }
 
-        if(id == null && department == null){
-            return  name + " - OWNER";
+        if (id == null && department == null) {
+            return name + " - OWNER";
         }
 
-        if(id != null && department == null){
-            return  "["+id+"] - " + name + " - OWNER";
+        if (id != null && department == null) {
+            return "[" + id + "] - " + name + " - OWNER";
         }
-        return "["+id+"] - " + name + " - " + department.toUpperCase();
+        return "[" + id + "] - " + name + " - " + department.toUpperCase();
     }
 
     public static void main(String[] args) {
