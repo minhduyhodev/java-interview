1748af566dbaa1a7e57a8226517d63d47faf7b9f
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-30 23:18:32 +0700

    restore spamm comment in print method

--- src/main/java/TimfromMarketing.java
+++ src/main/java/TimfromMarketing.java
@@ -1,6 +1,7 @@
 public class TimfromMarketing {
 
     public String print(Integer id, String name, String department) {
+        // spamm commit
         if (id == null && department != null) {
             return name + " - " + department.toUpperCase();
         }
