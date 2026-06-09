6b008defc6d9d7a1c105235e7ab7a77d75ddea21
minhduyhodev <duyhm.ce190378@gmail.com>
2026-06-09 23:42:01 +0700

    remove Acronym class and its methods

--- src/main/java/Acronym.java
+++ src/main/java/Acronym.java
@@ -1,19 +1,3 @@
-class Acronym {
-    private final String acronym;
+public class Acronym {
 
-    Acronym(String phrase) {
-        StringBuilder result = new StringBuilder();
-        String[] words = phrase.split("[^A-Za-z]+");
-
-        for (String word : words) {
-            if (!word.isEmpty()) {
-                result.append(Character.toUpperCase(word.charAt(0)));
-            }
-        }
-        this.acronym = result.toString();
-    }
-
-    String get() {
-        return acronym;
-    }
 }
