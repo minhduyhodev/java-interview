b8aec3b4add8f972b9c6efdf4d70eaafd43a879c
minhduyhodev <duyhm.ce190378@gmail.com>
2026-07-04 23:57:33 +0700

    refactor: reorganize AppointmentScheduler class and its methods

--- src/main/java/AppointmentScheduler.java
+++ src/main/java/AppointmentScheduler.java
@@ -1,3 +1,34 @@
-public class AppointmentScheduler {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
+class AppointmentScheduler {
+
+    public LocalDateTime schedule(String appointmentDateDescription) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("M/d/yyyy HH:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, dateTimeFormatter);
+    }
+
+    public boolean hasPassed(LocalDateTime appointmentDate) {
+        LocalDateTime parsed = LocalDateTime.now();
+        return appointmentDate.isBefore(parsed);
+    }
+
+    public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
+        return appointmentDate.getHour() >= 12 && appointmentDate.getHour() < 18;
+    }
+
+    public String getDescription(LocalDateTime appointmentDate) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a.",
+                Locale.ENGLISH);
+        String result = appointmentDate.format(dateTimeFormatter);
+        return "You have an appointment on " + result;
+    }
+
+    public LocalDate getAnniversaryDate() {
+        int year = LocalDate.now().getYear();
+        return LocalDate.of(year, 9, 15);
+    }
 }
